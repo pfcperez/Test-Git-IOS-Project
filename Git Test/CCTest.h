421ec6438762d56ad0e80d993eb2ea255c04fb66
Ramiro Perez <pfcperez@gmail.com>
2014-04-09 21:35:11 -0500

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Ramiro Perez on 09/04/14.
+//  Copyright (c) 2014 Ramiro Perez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Ramiro Perez on 09/04/14.
+//  Copyright (c) 2014 Ramiro Perez. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
